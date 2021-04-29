c47fe049831b44249f8e4c20a2f96f844969b163
Buwaneka Sumanasekara <buwaneka@interlective.com>
2021-04-29 22:35:58 +0530

    user login function

---
+++ app/Http/Traits/UserTrait.php
@@ -0,0 +1,26 @@
+<?php
+namespace App\Http\Traits;
+use App;
+use App\Models\User;
+use stdClass;
+
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+use App\Http\Traits\EmailTrait;
+
+trait UserTrait
+{
+
+    
+    public function user_role_getUserRolePermissions($user_role)
+    {
+
+        $permissions = DB::select("select per.* from pm_permissionss per inner join um_user_role_has_permissions ur_per on per.id=ur_per.pm_permissions_id
+        where ur_per.um_user_role_id='" . $user_role . "'  order by `order_no` asc ");
+
+       
+
+        return $permissions;
+    }
+
+}
--- database/seeders/PermissionSeeder.php
+++ database/seeders/PermissionSeeder.php
@@ -17,21 +17,21 @@ class PermissionSeeder extends Seeder
     {
         $ar_permissions=[
             //admin permissions
-            ["id"=>1000,"permission"=>"Create/Edit Tender Categories","tab_name"=>"Tender Categories"],
-            ["id"=>1001,"permission"=>"Create Tender","tab_name"=>"Create Tender"],
-            ["id"=>1002,"permission"=>"List Unpublished/Draft Tenders","tab_name"=>"Draft Tenders"],
-            ["id"=>1003,"permission"=>"View Tender Bids","tab_name"=>"Tender Bids"],
-            ["id"=>1004,"permission"=>"Active/Block users","tab_name"=>"User Management"],
+            ["id"=>1000,"permission"=>"Create/Edit Tender Categories","tab_name"=>"Tender Categories",'order_no'=>1],
+            ["id"=>1001,"permission"=>"Create Tender","tab_name"=>"Create Tender",'order_no'=>2],
+            ["id"=>1002,"permission"=>"List Unpublished/Draft Tenders","tab_name"=>"Draft Tenders",'order_no'=>3],
+            ["id"=>1003,"permission"=>"View Tender Bids","tab_name"=>"Tender Bids",'order_no'=>4],
+            ["id"=>1004,"permission"=>"Active/Block users","tab_name"=>"User Management",'order_no'=>5],
 
-            ["id"=>1100,"permission"=>"Profile details of user","tab_name"=>"My Profile"],
-            ["id"=>1101,"permission"=>"Change Password","tab_name"=>"Change Password"],
+            ["id"=>1100,"permission"=>"Profile details of user","tab_name"=>"My Profile",'order_no'=>10],
+            ["id"=>1101,"permission"=>"Change Password","tab_name"=>"Change Password",'order_no'=>11],
 
             //vendor permissions
-            ["id"=>2000,"permission"=>"View all bids of user","tab_name"=>"View My Bids"],
-            ["id"=>2001,"permission"=>"View all approved of user","tab_name"=>"Approved Bids"],
+            ["id"=>2000,"permission"=>"View all bids of user","tab_name"=>"View My Bids",'order_no'=>2000],
+            ["id"=>2001,"permission"=>"View all approved of user","tab_name"=>"Approved Bids",'order_no'=>2001],
             
-            ["id"=>2100,"permission"=>"Update Profile details of user","tab_name"=>"My Profile"],
-            ["id"=>2101,"permission"=>"Change Password","tab_name"=>"Change Password"],
+            ["id"=>2100,"permission"=>"Update Profile details of user","tab_name"=>"My Profile",'order_no'=>2002],
+            ["id"=>2101,"permission"=>"Change Password","tab_name"=>"Change Password",'order_no'=>2003],
         ];
 
         foreach ($ar_permissions as $permission) {
