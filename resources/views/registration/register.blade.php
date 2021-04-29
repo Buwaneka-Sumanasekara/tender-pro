861a725822c748043ef68a7cbef9a4a053e31bcd
Buwaneka Sumanasekara <buwaneka@interlective.com>
2021-04-29 23:29:12 +0530

    update

--- resources/views/registration/register.blade.php
+++ resources/views/registration/register.blade.php
@@ -19,40 +19,25 @@
 
 <body class="gray-bg">
     <div class="container">
-        <div class="row">
-        <div class="col-lg-6 m-l " >
-    <div class="text-center animated fadeInDown ">
-        <div>
-            <div>
-
-
+        <div class="row justify-content-md-center">
+        <div class="col-md-5 col-md-auto" >
+            <div class="text-center animated fadeInDown ">
                 <img src="images/logo-only.png" style="width: 30%;height: 30%"> <br><br>
+                    <div class="ibox">
+                        <div class="ibox-content">
+                            <form class="m-t">
+                                <div class="form-group  row"><label class="col-sm-2 col-form-label">Normal</label>
+                                    <div class="col-sm-10"><input type="text" class="form-control" style="background-image: url(&quot;data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABAAAAASCAYAAABSO15qAAAAAXNSR0IArs4c6QAAAPhJREFUOBHlU70KgzAQPlMhEvoQTg6OPoOjT+JWOnRqkUKHgqWP4OQbOPokTk6OTkVULNSLVc62oJmbIdzd95NcuGjX2/3YVI/Ts+t0WLE2ut5xsQ0O+90F6UxFjAI8qNcEGONia08e6MNONYwCS7EQAizLmtGUDEzTBNd1fxsYhjEBnHPQNG3KKTYV34F8ec/zwHEciOMYyrIE3/ehKAqIoggo9inGXKmFXwbyBkmSQJqmUNe15IRhCG3byphitm1/eUzDM4qR0TTNjEixGdAnSi3keS5vSk2UDKqqgizLqB4YzvassiKhGtZ/jDMtLOnHz7TE+yf8BaDZXA509yeBAAAAAElFTkSuQmCC&quot;); background-repeat: no-repeat; background-attachment: scroll; background-size: 16px 18px; background-position: 98% 50%; cursor: auto;"></div>
+                                </div>
+                                <button type="submit" class="btn btn-success block full-width m-b">LOGIN</button>
+                                <a href="#" style="color: gray"><small>Forgot password?</small></a>
+                                <p class="text-muted text-center"><small>Do not have an account?</small></p>
+                                <a class="btn btn-sm btn-danger btn-block">Create an account</a>
+                            </form>
+                        </div>
+                    </div>
             </div>
-            <div class="ibox">
-                <div class="ibox-content">
-
-            <form class="m-t">
-
-                <div class="form-group  row"><label class="col-sm-2 col-form-label">Normal</label>
-
-                    <div class="col-sm-10"><input type="text" class="form-control" style="background-image: url(&quot;data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABAAAAASCAYAAABSO15qAAAAAXNSR0IArs4c6QAAAPhJREFUOBHlU70KgzAQPlMhEvoQTg6OPoOjT+JWOnRqkUKHgqWP4OQbOPokTk6OTkVULNSLVc62oJmbIdzd95NcuGjX2/3YVI/Ts+t0WLE2ut5xsQ0O+90F6UxFjAI8qNcEGONia08e6MNONYwCS7EQAizLmtGUDEzTBNd1fxsYhjEBnHPQNG3KKTYV34F8ec/zwHEciOMYyrIE3/ehKAqIoggo9inGXKmFXwbyBkmSQJqmUNe15IRhCG3byphitm1/eUzDM4qR0TTNjEixGdAnSi3keS5vSk2UDKqqgizLqB4YzvassiKhGtZ/jDMtLOnHz7TE+yf8BaDZXA509yeBAAAAAElFTkSuQmCC&quot;); background-repeat: no-repeat; background-attachment: scroll; background-size: 16px 18px; background-position: 98% 50%; cursor: auto;"></div>
-
-                </div>
-
-
-
-                <button type="submit" class="btn btn-success block full-width m-b">LOGIN</button>
-
-                <a href="#" style="color: gray"><small>Forgot password?</small></a>
-                <p class="text-muted text-center"><small>Do not have an account?</small></p>
-                <a class="btn btn-sm btn-danger btn-block">Create an account</a>
-            </form>
         </div>
-    </div>
-
-        </div>
-    </div>
-    </div>
         </div>
     </div>
     <!-- Mainly scripts -->
