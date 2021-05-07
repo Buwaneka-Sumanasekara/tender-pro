7d47d46be89545db0ee34244609dbd62c91264b4
Sulari Ruwanthika <sulariruwanthika@gmail.com>
2021-05-07 19:01:36 +0530

    profile date uopdate done

--- app/Models/UmUser.php
+++ app/Models/UmUser.php
@@ -10,16 +10,15 @@ class UmUser extends Model
     use HasFactory;
 
     protected $table = 'um_user';
-    protected $fillable = ['id','firstname','lastname','um_user_status_id','um_user_role_id'];
+    protected $fillable = ['id', 'firstname', 'lastname', 'um_user_status_id', 'um_user_role_id'];
     public $incrementing = false;
-    
+
     public function vendor()
     {
-        if($this->um_user_role_id===config("global.user_role_admin")){
+        if ($this->um_user_role_id === config("global.user_role_admin")) {
             return null;
         }
-        return $this->belongsTo(VmVendor::class, 'um_user_id',"id");
+        return $this->hasOne(VmVendor::class, 'um_user_id', "id");
     }
 
-    
 }
